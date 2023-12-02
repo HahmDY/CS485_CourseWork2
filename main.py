ec0a36cbd6ebbfea37019c57638b8bdcd5bdae3c
youngmin yoo <yym3055@gmail.com>
2023-12-03 02:46:48 +0900

    Q1 done

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ import cv2 as cv
 import numpy as np
 import random
 from vectorQuantization import VectorQuantization
+from sklearn.ensemble import RandomForestClassifier
 
 
 def main():
     root = "./RF_code/Caltech_101/101_ObjectCategories"
     codebook_path = "./"
     vq = VectorQuantization(root)
-    vq.fit_codebook()
-    vq.save_codebook()
-    vq.construct_train_histograms()
+    # vq.fit_codebook()
+    # vq.save_codebook()
+    # vq.construct_train_histograms()
 
     # load codebook
     vq_new = VectorQuantization(root)
-    vq_new.load_codebook()
+    vq_new.load_codebook("./codebook.npy")
     vq_new.construct_train_histograms()
+    vq_new.construct_test_histograms()
 
-    # # visualize
-    # cls = "water_lilly"  # ['water_lilly', 'trilobite', 'wild_cat', 'wrench', 'wheelchair', 'yin_yang', 'umbrella', 'watch', 'windsor_chair', 'tick'
-    # idx = 2  # 0~14
+    # visualize
+    cls = "water_lilly"  # ['water_lilly', 'trilobite', 'wild_cat', 'wrench', 'wheelchair', 'yin_yang', 'umbrella', 'watch', 'windsor_chair', 'tick'
+    idx = 5  # 0~14
+    vq_new.visualization("train", cls, idx)
+
     # datahandler.visualization(True, cls, idx)  # train : True, test : False
 
     # # save result
